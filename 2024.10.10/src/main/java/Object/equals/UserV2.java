26de68f69050090ecd4abd47b53ed4d6df428b12
[SeokHwanJeongKR] <[jsver12@gmail.com]>
2024-10-17 09:02:39 +0900

    no message

--- 2024.10.10/src/main/java/Object/equals/UserV2.java
+++ 2024.10.10/src/main/java/Object/equals/UserV2.java
@@ -1,5 +1,7 @@
 package Object.equals;
 
+import java.util.Objects;
+
 public class UserV2 {
 
     private String id;
@@ -8,9 +10,19 @@ public class UserV2 {
         this.id = id;
     }
 
-    @Override
+   /* @Override
     public boolean equals(Object obj) {
         UserV2 user = (UserV2) obj;  // 메서드는 Object 타입을 매개 변수로 받기에 이것을 (UserV2) 타입으로 캐스팅 해준다
         return this.id.equals(user.id);
+    }*/
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserV2 userV2 = (UserV2) o;
+        return Objects.equals(id, userV2.id);
     }
+
+
 }
